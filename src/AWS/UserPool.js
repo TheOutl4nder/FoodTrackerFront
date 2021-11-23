06f36bb33848997ebcdb34d5c790cc0dde1bfde4
kmoreno <kmoreno@infobip.com>
2021-11-23 10:26:40 -0600

    added .env file

---
+++ src/AWS/UserPool.js
@@ -0,0 +1,10 @@
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+
+console.log(process.env.REACT_APP_USER_POOL_ID);
+
+const poolData = {
+    UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+    ClientId:process.env.REACT_APP_CLIENT_ID
+}
+
+export default new CognitoUserPool(poolData);
